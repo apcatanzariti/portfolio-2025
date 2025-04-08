553466bca3cee0f41bfab901e6e1e77a107d51fc
Anthony Catanzariti <anthonycatanzariti@Anthonys-MacBook-Pro.local>
2025-04-08 17:21:45 -0400

    got smooth scrolling to anchors to work

--- portfolio-app/src/app/page.tsx
+++ portfolio-app/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useEffect, useState } from "react";
 import Header from "..//components/layout/Header/Header";
 import About from "../components/layout/About/About";
 import Projects from "../components/layout/Projects/Projects";
@@ -6,21 +8,62 @@ import Footer from "..//components/layout/Footer/Footer";
 import "./globals.css";
 
 export default function Homepage() {
+  const [mounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const goToTop = document.getElementById("header");
+    const goToAbout = document.getElementById("about");
+    const goToProjects = document.getElementById("projects");
+    const goToContact = document.getElementById("contact");
+
+    if (goToTop && goToAbout && goToProjects && goToContact) {
+      setIsMounted(true);
+    };
+  }, []);
+
+  function scrollToTop() {
+    if (mounted) {
+      const goToTop = document.getElementById("header");
+      goToTop?.scrollIntoView({ behavior: "smooth", block: "start" });
+    };
+  };
+
+  function scrollToAbout() {
+    if (mounted) {
+      const goToAbout = document.getElementById("about");
+      goToAbout?.scrollIntoView({ behavior: "smooth", block: "start" });
+    };
+  };
+
+  function scrollToProjects() {
+    if (mounted) {
+      const goToProjects = document.getElementById("projects");
+      goToProjects?.scrollIntoView({ behavior: "smooth", block: "start" });
+    };
+  };
+
+  function scrollToContact() {
+    if (mounted) {
+      const goToContact = document.getElementById("contact");
+      goToContact?.scrollIntoView({ behavior: "smooth", block: "start" });
+    };
+  };
+
   return (
     <div>
       <header>
         <section id="header">
-          <Header />
+          <Header scrollToTop={scrollToTop} scrollToAbout={scrollToAbout} scrollToProjects={scrollToProjects} scrollToContact={scrollToContact} />
         </section>
       </header>
       <main>
-        <section id="about">
+        <section id="about" className="section-padding">
           <About />
         </section>
-        <section id="projects">
+        <section id="projects" className="section-padding">
           <Projects />
         </section>
-        <section id="contact">
+        <section id="contact" className="section-padding">
           <Contact />
         </section>
       </main>
--- portfolio-app/src/components/layout/Header/Header.tsx
+++ portfolio-app/src/components/layout/Header/Header.tsx
@@ -1,26 +1,35 @@
 import "./header.css";
 // import Image from "next/image";
 
-export default function Header() {
+interface HeaderProps {
+    scrollToTop: () => void;
+    scrollToAbout: () => void;
+    scrollToProjects: () => void;
+    scrollToContact: () => void;
+};
+
+export default function Header({ scrollToTop, scrollToAbout, scrollToProjects, scrollToContact }: HeaderProps) {
     return (
-        <div className="header-container">
-            <div className="logo-title-container">
-                {/* <div className="header-image-container">
-                    <Image
-                        src="/header-image.png"
-                        alt="Anthony Catanzariti Logo"
-                        width={60}
-                        height={70}
-                        style={{  display: "block", margin: "0 auto" }}
-                    />
-                </div> */}
-                <p className="title-text">Anthony Catanzariti</p>
-            </div>
-            <div className="navigation-container">
-                <p>Home</p>
-                <p><a href="./#about">About</a></p>
-                <p>Projects</p>
-                <p>Contact</p>
+        <div className="header">
+            <div className="header-container">
+                <div className="logo-title-container">
+                    {/* <div className="header-image-container">
+                        <Image
+                            src="/header-image.png"
+                            alt="Anthony Catanzariti Logo"
+                            width={60}
+                            height={70}
+                            style={{  display: "block", margin: "0 auto" }}
+                        />
+                    </div> */}
+                    <p className="title-text">Anthony Catanzariti</p>
+                </div>
+                <div className="navigation-container">
+                    <p onClick={scrollToTop}>Home</p>
+                    <p onClick={scrollToAbout}>About</p>
+                    <p onClick={scrollToProjects}>Projects</p>
+                    <p onClick={scrollToContact}>Contact</p>
+                </div>
             </div>
         </div>
     );
