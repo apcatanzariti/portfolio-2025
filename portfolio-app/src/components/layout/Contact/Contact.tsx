35a1c9045bc0a8805e2c877c4f14fd3b3625fa62
Anthony Catanzariti <anthonycatanzariti@Anthonys-MacBook-Pro.local>
2025-04-14 19:30:32 -0400

    email function now works and footer done. only need to add more projects.

--- portfolio-app/src/components/layout/Contact/Contact.tsx
+++ portfolio-app/src/components/layout/Contact/Contact.tsx
@@ -1,7 +1,27 @@
-import SmallButton from "../../ui/small-button/SmallButton";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 export default function Contact() {
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        if (form.current) {
+          emailjs.sendForm("service_t1did17", "template_c7vob2r", form.current, { publicKey: "8FxreZAX5r_UhSMol" })
+            .then(() => {
+              setFormSubmitted(true);
+            }, () => {
+              alert(`Error sending email. Please try again.`);
+            }
+          );
+        } else {
+          alert("Form reference is not available.");
+        };
+    };
+
     return (
         <div className="contact-container">
             <div className="contact-title">
@@ -12,7 +32,8 @@ export default function Contact() {
                 <p>Feel free to reach out by filling out the form below and I will respond as soon as I can</p>
             </div>
             <div className="contact-form-container">
-                <form className="contact-form">
+                {!formSubmitted ? (
+                <form className="contact-form" ref={form} onSubmit={sendEmail}>
                     <div className="input-container">
                         <label className="form-label" htmlFor="name">Name</label>
                         <input className="form-input" type="text" id="name" name="name" required />
@@ -25,8 +46,11 @@ export default function Contact() {
                         <label className="form-label" htmlFor="message">Message</label>
                         <textarea className="form-input" id="message" name="message" cols={30} rows={8} required></textarea>
                     </div>
-                    <SmallButton onClick={() => { const e = window.event; if (e) e.preventDefault(); }} buttonText="Submit" />
+                    <button className="email-button">Submit</button>
                 </form>
+                ) : (
+                    <div style={{ textAlign: "center", fontWeight: "bold" }}>Thank you for reaching out! I will respond as soon as I can.</div>
+                )}
             </div>
         </div>
     );
